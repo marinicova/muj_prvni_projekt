1b5d805d6bd64c6970bc4ff28185c6b0b887a1fb
marinicova <janka.pasakova@gmail.com>
2020-09-27 21:43:47 +0200

    hotový projekt

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,29 +1,3 @@
-# Prihlaseni do aplikace.
-oddelovac = '-' * 60
-print(oddelovac)
-# 1.Na začátku přivítá uživatele.
-print('Vitej v naši aplikaci. Prosím přihlašte se:')
-
-# 2. Vyžádá si od uživatele přihlašovací jméno a heslo.
-jmeno = str(input('Jméno: '))
-heslo = str(input('Heslo: '))
-uzivatele = {'bob': '123', 'ann': 'pass123', 'mike': 'password123', 'liz': 'pass123'}
-print(oddelovac)
-
-# 3. Zjistí, jestli zadané údaje odpovídají někomu z registrovaných uživatelů.
-if uzivatele.get(jmeno) != heslo:
-    print('špátné jméno nebo heslo!!!')
-    print(oddelovac)
-    exit()
-else:
-    print('Si přihlášen :)')
-    print(oddelovac)
-
-# 4. Program nechá uživatele vybrat mezi třemi texty, uloženými v proměnné TEXTS.
-print('Máme 3 texty k analyzovaní.')
-vyber = int(input('Prosím vložte číslo od 1 do 3: '))
-print(oddelovac)
-
 texts = ['''
 Situated about 10 miles west of Kemmerer, 
 Fossil Butte is a ruggedly impressive 
@@ -54,60 +28,65 @@ in modern oceans. Other fish such as paddlefish,
 garpike and stingray are also present.'''
 ]
 
-# Analyzuje text
-# 5. Pro vybraný text spočítá následující statistiky:
-# a - počet slov,
-# b - počet slov začínajících velkým písmenem,
-# c - počet slov psaných velkými písmeny,
-# d - počet slov psaných malými písmeny,
-# e - počet čísel (ne cifer!).
+oddelovac = '-' * 60
+print(oddelovac)
+print('Vitej v naši aplikaci. Prosím přihlašte se:')
 
-# Úprava textu
+jmeno = input('Jméno: ')
+heslo = input('Heslo: ')
+uzivatele = {'bob': '123', 'ann': 'pass123', 'mike': 'password123', 'liz': 'pass123'}
+print(oddelovac)
+
+if uzivatele.get(jmeno) != heslo:
+    print('špátné jméno nebo heslo!!!')
+    print(oddelovac)
+    exit()
+else:
+    print('Si přihlášen :)')
+    print(oddelovac)
+
+print('Máme 3 texty k analyzování.')
+vyber = int(input('Prosím vložte číslo od 1 do 3: '))
+print(oddelovac)
+
 text = texts[vyber - 1]
 text_o = [slovo.strip(",.") for slovo in text.split()]
 
-# a
 slova = len(text_o)
 velke_pp = 0
 velke_p = 0
 male_p = 0
 cislice = 0
-for znak in text_o:
-    # b
-    if znak.istitle():
+
+for slovo in text_o:
+
+    if slovo.istitle():
         velke_pp += 1
-        continue
-    # c
-    if znak.isupper():
+
+    elif slovo.isupper():
         velke_p += 1
-    # d
-    elif znak.islower():
+
+    elif slovo.islower():
         male_p += 1
-    # e
-    elif znak.isnumeric():
+
+    elif slovo.isnumeric():
         cislice += 1
 
-# Vypsání analyzy textu
-print(f"V texte se nachází  {slova} slov.")
+print(f"V texte se nachází {slova} slov.")
 print(f"Daný text obsahuje {velke_pp} slov začínajících velkým písmenem.")
-print(f"Daný text obsahuje  {velke_p} slov psaných velkými písmeny.")
+print(f"Daný text obsahuje {velke_p} slov psaných velkými písmeny.")
 print(f"Daný text obsahuje {male_p} slov psaných malými písmeny.")
 print(f"Daný text obsahuje {cislice} čísla.")
 print(oddelovac)
 
-# 6. Program zobrazí jednoduchý sloupcový graf, který bude reprezentovat četnost různých délek slov v textu.
-pocet_pismen_slovo = {}
+delka_slovo = {}
 for slovo in text_o:
-    pocet_pismen_slovo[len(slovo)] = pocet_pismen_slovo.get(len(slovo), 0) + 1
-od_nejmensiho = sorted(pocet_pismen_slovo.keys(), reverse=False)
-for klic in od_nejmensiho:
-    print(f"{klic} {'*'* pocet_pismen_slovo.get(klic)} {pocet_pismen_slovo.get(klic)}")
+    delka_slovo[len(slovo)] = delka_slovo.get(len(slovo), 0) + 1
+
+for klic in sorted(delka_slovo.keys()):
+    print(f"{klic} {'*'* delka_slovo.get(klic)} {delka_slovo.get(klic)}")
 print(oddelovac)
 
-# 7. Program spočítá součet všech čísel (ne cifer!) v textu.
-cisla = 0
-for slovo in text_o:
-    if slovo.isnumeric():
-        cisla = cisla + int(slovo)
+cisla = sum([int(slovo) for slovo in text_o if slovo.isnumeric()])
 print(f"Když spočítame všechny čísla v textě dostaneme: {str(cisla)}")
 print(oddelovac)
